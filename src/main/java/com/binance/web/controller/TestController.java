933775a2688302b2c0bf98efebb3e36adda8f150
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2025-03-04 01:59:54 -0500

    test

--- src/main/java/com/binance/web/controller/TestController.java
+++ src/main/java/com/binance/web/controller/TestController.java
@@ -1,5 +1,16 @@
 package com.binance.web.controller;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
