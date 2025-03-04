b8d28b1d2344e9547a1262f55fef6cec3d03e923
MaicolRuiz01 <106454008+MaicolRuiz01@users.noreply.github.com>
2025-03-04 01:57:06 -0500

    Create TestController.java

---
+++ src/main/java/com/binance/web/controller/TestController.java
@@ -0,0 +1,17 @@
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping("/binance-time")
+    public ResponseEntity<String> testBinanceConnection() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.binance.com/api/v3/time"))
+                .GET()
+                .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            return ResponseEntity.ok(response.body());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+        }
+    }
+}
