d586863981533798a54b89e104b96a5b65f0fcec
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2025-03-05 23:15:01 -0500

    controlador de ip

---
+++ src/main/java/com/binance/web/controller/IPController.java
@@ -0,0 +1,30 @@
+package com.binance.web.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class IPController {
+    @GetMapping("/my-ip")
+    public String getServerIp() throws IOException {
+        URL url = new URL("https://api64.ipify.org?format=json");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        return content.toString();
+    }
+}
