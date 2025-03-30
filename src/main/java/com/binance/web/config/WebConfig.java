58d74f83c0703b74eb7e4f6f929a08c80522e569
Mauricio <jonathanmauriciovabo@ufps.edu.co>
2025-03-29 19:27:47 -0500

    CORS error solution

--- src/main/java/com/binance/web/config/WebConfig.java
+++ src/main/java/com/binance/web/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/*")
                 .allowedOrigins("*") // Cambia esto por la URL de tu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
