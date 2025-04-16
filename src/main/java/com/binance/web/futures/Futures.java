95575747c99ea2b783ba399d3bc54cd75fdfdd84
Titsu-Maki <cristianalejandrovecr@ufps.edu.co>
2025-04-16 00:21:12 -0500

    Actualizo ZeusRama con cambios desde main

--- src/main/java/com/binance/web/futures/Futures.java
+++ src/main/java/com/binance/web/futures/Futures.java
@@ -1,5 +1,14 @@
 package com.binance.web.futures;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Futures {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
 }
